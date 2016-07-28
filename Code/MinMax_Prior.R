948c35cbfa5ed7124a45135c02dcb9034b88cd7c
Surya Tokdar <tokdar@stat.duke.edu>
2016-07-28 18:37:32 -0400

    Update MinMax_Prior.R
    Part of 'sigma2 prior change' move. The new prior on sigma2 is IG(r_0, r_0) truncated to (0.01, 9) with a small r_0 (= 0.01).

--- Code/MinMax_Prior.R
+++ Code/MinMax_Prior.R
@@ -1,15 +1,10 @@
-K.SE = K.SE.inv = list()
+K.SE = K.SE.chol = K.SE.inv = list()
 t.T = 40
+r2 <- as.matrix(dist(1:t.T))^2
 for(l in 1:L){
-  K.SE[[l]] = matrix(nrow=t.T,ncol=t.T)
-  for(t in 1:t.T){
-    for(s in 1:t.T){
-      r = abs(t-s)
-      K.SE[[l]][t,s] = exp(-r^2/(2*ell[l]^2) ) # Squared exponential
-    }
-  }
-  K.SE[[l]] = K.SE[[l]] + .01*diag(40) 
-  K.SE.inv[[l]] = solve(K.SE[[l]])
+  K.SE[[l]] = exp(-r2/(2*ell[l]^2) ) + diag(1e-12, t.T)
+  K.SE.chol[[l]] <- chol(K.SE[[l]])
+  K.SE.inv[[l]] <- chol2inv(K.SE.chol[[l]])
 }
 
 eta = matrix(nrow = nSamples,ncol = t.T)
@@ -18,7 +13,7 @@ for(i in 1:nSamples){
   m_gamma = rnorm(K,0,1)
   sigma2_gamma = rinvgamma(K,shape = r_gamma,scale = s_gamma)
   k = sample(1:K,size=1,prob=pi_gamma)
-  sigma2 = rinvgamma(1,shape = r_0, scale = s_0)
+  sigma2 = 1/rtruncgamma(1,shape = r_0, scale = s_0, low = 1/9, up = 1e4)
   
   eta_bar = rnorm(1,m_gamma[k], sqrt(sigma2_gamma[k]))
   
